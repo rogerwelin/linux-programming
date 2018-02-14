92d0a0e96227bf425af2c2d92632dad668927d36
rogerwelin <roger.welin@icloud.com>
2018-02-14 23:42:22 +0100

    make

--- lib/tlpi_hdr.h
+++ lib/tlpi_hdr.h
@@ -1,5 +1,5 @@
 #ifndef TLPI_HDR_H
-#define TIPI_HDR_H      /* prevent accidental double inclusion */
+#define TLPI_HDR_H      /* prevent accidental double inclusion */
 
 #include <sys/types.h>  /* type definitions used by many programs */
 #include <stdio.h>      /* standard I/O functions */
@@ -7,9 +7,9 @@
                            plus EXIT_SUCCESS and EXIY FAILURE constants */
 #include <unistd.h>     /* prototypes for many system calls */
 #include <errno.h>      /* declares errno and defines error constants */
-#include <sting.h>      /* commonly used string-handling functions */
+#include <string.h>      /* commonly used string-handling functions */
 
-#include "get_num-h"    /* declares our functions for handling numeric arguments  */
+#include "get_num.h"    /* declares our functions for handling numeric arguments  */
 
 #include <error_functions.h>  /* declares our error-handling functions */
 
