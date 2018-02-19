dfa6b3f18b5e3159a5e3c6b14cdf6fc5c12f98dc
rogerwelin <roger.welin@icloud.com>
2018-02-19 22:31:20 +0100

    more stuff

---
+++ syslimit/t_sysconf.c
@@ -0,0 +1,31 @@
+#include "tlpi_hdr.h"
+
+
+/* print msg plus sysconf() value for name */
+static void sysconfPrint(const char *msg, int name) {
+  long lim;
+
+  errno = 0;
+  lim = sysconf(name);
+  if (lim != -1) {                  /* call succeeded, limit determined */
+    printf("%s %ld\n", msg, lim);
+  } else {
+    if (errno == 0)                 /* Call succeeded, limit indeterminate */
+      printf("%s (indeterminate)\n", msg);
+    else
+      errExit("sysconf %s", msg);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  sysconfPrint("_SC_ARG_MAX:         ", _SC_ARG_MAX);
+  sysconfPrint("_SC_LOGIN_NAME_MAX:  ", _SC_LOGIN_NAME_MAX);
+  sysconfPrint("_SC_OPEN_MAX:        ", _SC_OPEN_MAX);
+  sysconfPrint("_SC_NGROUPS_MAX:     ", _SC_NGROUPS_MAX);
+  sysconfPrint("_SC_PAGESIZE:        ", _SC_PAGESIZE);
+  sysconfPrint("_SC_RTSIG_MAX:       ", _SC_RTSIG_MAX);
+
+  exit(EXIT_SUCCESS);
+
+
+}
